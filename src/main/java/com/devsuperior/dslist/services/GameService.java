dfda4a5b338726afaca2538139484935c4c76a2b
italocostta <italo.costta26@gmail.com>
2023-11-15 16:58:28 -0300

    Projection

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,9 @@ public class GameService {
         return result.stream().map(GameMinDTO::new).toList();
     }
 
-
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listID) {
+        List<GameMinProjection> result = gameRepository.searchByList(listID);
+        return result.stream().map(GameMinDTO::new).toList();
+    }
 }
